51632dbd1f3e83a1ad0bf47a9c243fa309f1d6fb
Olena-Nechyporenko <olenan0309@gmail.com>
2023-08-08 15:49:56 +0300

    added library vimeo player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
 
+
+
+
+
+
+
+
+
+
+
+
+
+// const iframe = document.querySelector('#vimeo-player');
+//     const player = new Player(iframe);
+
+    // player.on('play', function() {
+    //     console.log('played the video!');
+    // });
+
+    // const onPlay = function(data) {
+    //     // data is an object containing properties specific to that event
+    // };
+    
+    // player.on('timeupdate', onPlay);
+
+    // player.on('timeupdate', function(data) {
+	// 	console.log(data.duration)
+    // })
